52b18d76531639a7f6e5aeaa16fb0b5da0f49931
TheresManuela <158140242+TheresManuela@users.noreply.github.com>
2024-02-28 09:17:09 +0530

    Add files via upload

---
+++ largesmalllist.py
@@ -0,0 +1,14 @@
+def find_largest_and_smallest(list1):
+  
+# list of numbers
+
+# sorting the list
+ list1.sort()
+
+# printing the first element
+ print("Smallest number is", list1[0])
+
+ print("Largest number is", list1[-1])
+
+list1 = [10, 20, 4, 45, 99]
+find_largest_and_smallest(list1)
---
+++ swapvariable.py
@@ -0,0 +1,7 @@
+def swap_variables(a,b):
+    return b,a
+x=10
+y=50
+x,y=swap_variables(x,y)
+print("x=", x)
+print("y=", y)
